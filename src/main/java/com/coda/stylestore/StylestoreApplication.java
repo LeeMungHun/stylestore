3d0aa6d0a2d71b355f98539d1e0b401179fa6fb4
lee <ltenamunelbo@gmail.com>
2022-08-02 12:01:54 +0900

    Initial commit

---
+++ src/main/java/com/coda/stylestore/StylestoreApplication.java
@@ -0,0 +1,13 @@
+package com.coda.stylestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coda/stylestore/StylestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coda.stylestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
