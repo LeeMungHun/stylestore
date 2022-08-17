f82c20b03c8cb69a65f821bd513b2b6af423b280
lee <ltenamunelbo@gmail.com>
2022-08-17 17:50:56 +0900

    구글로그인 기능 구현

--- src/main/java/com/coda/stylestore/web/IndexController.java
+++ src/main/java/com/coda/stylestore/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.coda.stylestore.web;
 
+import com.coda.stylestore.config.auth.dto.SessionUser;
 import com.coda.stylestore.service.posts.PostsService;
 import com.coda.stylestore.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        // CustomOAuth2UserService 로그인 성공시 SessionUser를 세션에 저장
+        SessionUser user = (SessionUser) httpSession
+                .getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("username", user.getName());
+        }
         return "index";
     }
 
